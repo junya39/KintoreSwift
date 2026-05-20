c300ce5b401002cd5a2e6d26140af59c9994788a
jun <75928740+junya39@users.noreply.github.com>
2026-05-20 19:18:38 +0900

    MonsterDexの画像と詳細データを追加

--- KintoreSwift/Models/Monster.swift
+++ KintoreSwift/Models/Monster.swift
@@ -1,11 +1,35 @@
 import Foundation
 
+enum MonsterType: String, Codable, Equatable {
+    case power
+    case endurance
+    case balanced
+    case habit
+    case special
+
+    var displayName: String {
+        switch self {
+        case .power:
+            return "Power"
+        case .endurance:
+            return "Endurance"
+        case .balanced:
+            return "Balanced"
+        case .habit:
+            return "Habit"
+        case .special:
+            return "Special"
+        }
+    }
+}
+
 struct Monster: Identifiable, Codable, Equatable {
     let id: String
     let number: Int
     let imageName: String
     let name: String
     let nickname: String
+    let type: MonsterType
     let evolutionStage: Int
     let description: String
     let unlockCondition: String
@@ -13,6 +37,10 @@ struct Monster: Identifiable, Codable, Equatable {
 
     var displayNumber: String {
         String(format: "#%03d", number)
+    }
+
+    var stageDisplayName: String {
+        "Stage \(evolutionStage)"
     }
 }
 
